dbdadcd32f875b89c158f3cf466f1334b6ec8475
zhangyan1298 <38239794+zhangyan1298@users.noreply.github.com>
2018-08-16 09:45:41 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,13 @@
  docker pull mongoDB:2.8.0
  docker pull zookeeper:3.4.11
 wget http://bkopen-10032816.file.myqcloud.com/cmdb/cmdb.tar.gz
+
+#配置redis config
+#导入redis.conf configMap
+#kubectl delete configmap redisconfg -n prod 删除configMap
+ kubectl create configmap redisconfg --from-file redis.conf -n prod
+#配置redis AUTH
+bind 0.0.0.0
+requirepass password
+###
+
